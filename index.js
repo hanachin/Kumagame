46850c2099742930421a90de55aa6f637bee36d5
Seiei Higa <hanachin@gmail.com>
2014-11-13 22:55:19 +0900

    beautify!
    $ npm install -g js-beautify
    $ js-beautify index.js > index-beauty.js
    $ mv index-beauty.js index.js

--- index.js
+++ index.js
@@ -7,91 +7,103 @@ var game = null;
 
 
 var KumaSprite = Class.create(Sprite, {
-    
+
     initialize: function() {
-	Sprite.call(this, 32, 32);	
-	
-	this.image = game.assets[CHARA_IMAGE_NAME];	
-	var deg = Math.random()*360*Math.PI/180;
-	this.vx = Math.cos(deg);
-	this.vy = Math.sin(deg);
+        Sprite.call(this, 32, 32);
+
+        this.image = game.assets[CHARA_IMAGE_NAME];
+        var deg = Math.random() * 360 * Math.PI / 180;
+        this.vx = Math.cos(deg);
+        this.vy = Math.sin(deg);
     },
-    
-    
+
+
     onenterframe: function() {
-	
-	this.moveBy(this.vx*4, this.vy*4);
-	
-	var left = 0;
-	var right = game.width-this.width;
-	var top = 0;
-	var bottom = game.height-this.height;
-	if (this.x < left)   { this.x = left;   this.vx*=-1; }
-	if (this.x > right)  { this.x = right;  this.vx*=-1; }
-	if (this.y < top)    { this.y = top;    this.vy*=-1; }
-	if (this.y > bottom) { this.y = bottom; this.vy*=-1; }
+
+        this.moveBy(this.vx * 4, this.vy * 4);
+
+        var left = 0;
+        var right = game.width - this.width;
+        var top = 0;
+        var bottom = game.height - this.height;
+        if (this.x < left) {
+            this.x = left;
+            this.vx *= -1;
+        }
+        if (this.x > right) {
+            this.x = right;
+            this.vx *= -1;
+        }
+        if (this.y < top) {
+            this.y = top;
+            this.vy *= -1;
+        }
+        if (this.y > bottom) {
+            this.y = bottom;
+            this.vy *= -1;
+        }
     },
-    
-    
+
+
     ontouchstart: function() {
-	this.parentNode.removeChild(this);
+        this.parentNode.removeChild(this);
     }
 });
 
 window.onload = function() {
     game = new Game();
-    game.preload(CHARA_IMAGE_NAME);	
+    game.preload(CHARA_IMAGE_NAME);
     game.fps = 30;
-    
+
     game.onload = function() {
-	var scene = game.rootScene;
-	scene.backgroundColor = "black";
+        var scene = game.rootScene;
+        scene.backgroundColor = "black";
 
-	var scoreLabel = null;
-	var timerLabel = null;
+        var scoreLabel = null;
+        var timerLabel = null;
 
-	game.frame = 0;
-	game.score = 0;
-	
-	scoreLabel = new Label();
-	scene.addChild(scoreLabel);
-	scoreLabel.moveTo(10, 10);
-	scoreLabel.color = "white";
-	scoreLabel.font = "11px'Consolas','Monaco','MS ゴシック'",
-	scoreLabel.text = "Score:"
+        game.frame = 0;
+        game.score = 0;
 
-	timerLabel = new Label();
-	scene.addChild(timerLabel);
-	timerLabel.moveTo(250,10);
-	timerLabel.color = "white"
-	timerLabel.font = "11px'Consolas','Monaco','MS ゴシック'",
-	timerLabel.text = "Timer:";
+        scoreLabel = new Label();
+        scene.addChild(scoreLabel);
+        scoreLabel.moveTo(10, 10);
+        scoreLabel.color = "white";
+        scoreLabel.font = "11px'Consolas','Monaco','MS ゴシック'",
+            scoreLabel.text = "Score:"
 
-	scene.onenterframe = function() {
+        timerLabel = new Label();
+        scene.addChild(timerLabel);
+        timerLabel.moveTo(250, 10);
+        timerLabel.color = "white"
+        timerLabel.font = "11px'Consolas','Monaco','MS ゴシック'",
+            timerLabel.text = "Timer:";
 
-	  scoreLabel.text = "Score:" + game.score;
+        scene.onenterframe = function() {
 
-	  var time = GAME_LIMIT_TIME - Math.floor(game.frame/game.fps);
-	  timerLabel.text = "Timer:" + time;
-	  if(time < 10) {
-	  timerLabel.color = "red";
-	}
-	
-	if(time <= 0) {
-	var score = game.score;
-	var message = game.score + "点獲得しました!";
-	alert(score + ',' + message);
-	scene.onenterframe = null;
-	}
-};
+            scoreLabel.text = "Score:" + game.score;
 
-	
-	for (var i=0; i<16; ++i) {
-	    var kuma = new KumaSprite();	
-	    kuma.moveTo( Math.random()*(game.width-kuma.width), Math.random()*(game.height-kuma.height) );
-	    scene.addChild(kuma);		
-	}
+            var time = GAME_LIMIT_TIME - Math.floor(game.frame / game.fps);
+            timerLabel.text = "Timer:" + time;
+            if (time < 10) {
+                timerLabel.color = "red";
+            }
+
+            if (time <= 0) {
+                var score = game.score;
+                var message = game.score + "点獲得しました!";
+                alert(score + ',' + message);
+                scene.onenterframe = null;
+            }
+        };
+
+
+        for (var i = 0; i < 16; ++i) {
+            var kuma = new KumaSprite();
+            kuma.moveTo(Math.random() * (game.width - kuma.width), Math.random() * (game.height - kuma.height));
+            scene.addChild(kuma);
+        }
     };
-    
+
     game.start();
 };
